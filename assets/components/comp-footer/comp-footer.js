fca9e7956638bbf523324524393e4d616185cdb9
Federico Deniard <fededeniard@gmail.com>
2023-12-25 02:05:39 -0300

    test page removed

--- assets/components/comp-footer/comp-footer.js
+++ assets/components/comp-footer/comp-footer.js
@@ -4,12 +4,13 @@ class compFooter extends HTMLElement{
     }
     connectedCallback(){
         this.innerHTML = 
-    `	<div class="footer">
-    <footer class="footer-container">
-        <a href="https://www.instagram.com/fededeniard/" target="_blank"><img src="../../assets/instagram.svg"></img><h4> instagram</h4></a>
-        <a href="https://www.youtube.com/channel/UCtFkBfAWMelIyY9xCVHUprw" target=".blank"><img src="../../assets/youtube.svg"></img><h4>youtube</h4></a>
-    </footer>
-</div><link rel="stylesheet" type="text/css" href="../../assets/components/comp-footer/comp-footer.css">`
+    `<link rel="stylesheet" type="text/css" href="../../assets/components/comp-footer/comp-footer.css">
+    <div class="footer">
+        <footer class="footer-container">
+                <a href="https://www.instagram.com/fededeniard/" target="_blank"><img src="../../assets/instagram.svg"></img><h4> instagram</h4></a>
+         <a href="https://www.youtube.com/channel/UCtFkBfAWMelIyY9xCVHUprw" target=".blank"><img src="../../assets/youtube.svg"></img><h4>youtube</h4></a>
+      </footer>
+    </div>`
     }
 }
 window.customElements.define("comp-footer", compFooter);
